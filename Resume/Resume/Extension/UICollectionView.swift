bf8da380d52b98c9f7b7dc7beeec30df21a46654
ROGER <>
2018-05-27 10:34:37 +0900

    add Extension - UITableView , UICollectionView

---
+++ Resume/Resume/Extension/UICollectionView.swift
@@ -0,0 +1,9 @@
+//
+//  UICollectionView.swift
+//  Resume
+//
+//  Created by ROGER on 2018. 5. 27..
+//  Copyright © 2018년 Hexcon. All rights reserved.
+//
+
+import Foundation
---
+++ Resume/Resume/Extension/UITableView.swift
@@ -0,0 +1,9 @@
+//
+//  UITableView.swift
+//  Resume
+//
+//  Created by ROGER on 2018. 5. 27..
+//  Copyright © 2018년 Hexcon. All rights reserved.
+//
+
+import Foundation
